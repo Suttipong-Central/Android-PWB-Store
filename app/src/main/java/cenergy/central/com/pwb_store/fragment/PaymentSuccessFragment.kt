d13e5df512b37ea4fa9681ab147145bf9c4ab104
Pawat <ohmygodood@gmail.com>
2018-08-20 16:47:31 +0700

    EO-2 Create PaymentSuccessFragment part1

---
+++ app/src/main/java/cenergy/central/com/pwb_store/fragment/PaymentSuccessFragment.kt
@@ -0,0 +1,34 @@
+package cenergy.central.com.pwb_store.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cenergy.central.com.pwb_store.R
+
+class PaymentSuccessFragment : Fragment(){
+
+    companion object {
+        fun newInstance(contactNo: String): ProductDetailFragment {
+            val fragment = ProductDetailFragment()
+            val args = Bundle()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_payment_success, container, false)
+        setupView(rootView)
+        return rootView
+    }
+
+    private fun setupView(rootView: View) {
+
+    }
+}
