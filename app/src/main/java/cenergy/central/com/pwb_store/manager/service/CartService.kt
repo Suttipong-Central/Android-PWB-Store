2dffe4f14e5f82fc988c4a61052cf45f060329d4
Pawat <ohmygodood@gmail.com>
2018-08-23 13:41:26 +0700

    Created CartService

---
+++ app/src/main/java/cenergy/central/com/pwb_store/manager/service/CartService.kt
@@ -0,0 +1,11 @@
+package cenergy.central.com.pwb_store.manager.service
+
+import retrofit2.Call
+import retrofit2.http.POST
+
+internal interface CartService {
+
+    @POST("/rest/V1/guest-carts")
+    fun createCart(): Call<String>
+
+}
