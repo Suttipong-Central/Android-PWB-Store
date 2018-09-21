3ce0d1bccc6a369de086581fdbe55e95fba30758
Pawat <ohmygodood@gmail.com>
2018-09-21 09:33:04 +0700

    Improve delivery option follow design [EO-54]

--- app/src/main/java/cenergy/central/com/pwb_store/adapter/viewholder/DeliveryOptionViewHolder.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/adapter/viewholder/DeliveryOptionViewHolder.kt
@@ -1,25 +1,28 @@
 package cenergy.central.com.pwb_store.adapter.viewholder
 
+import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import cenergy.central.com.pwb_store.R
 import cenergy.central.com.pwb_store.manager.listeners.DeliveryOptionsListener
 import cenergy.central.com.pwb_store.model.DeliveryOption
-import cenergy.central.com.pwb_store.view.PowerBuyTextView
+import cenergy.central.com.pwb_store.model.DeliveryType
 
-class DeliveryOptionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class DeliveryOptionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private var carrierTitle: TextView = itemView.findViewById(R.id.tv_carrier_title)
     private var methodTitle: TextView = itemView.findViewById(R.id.tv_method_title)
+    private var amount: TextView = itemView.findViewById(R.id.tv_amount)
 
+    @SuppressLint("SetTextI18n")
     fun bindItem(deliveryOption: DeliveryOption, listener: DeliveryOptionsListener?) {
-        carrierTitle.text = deliveryOption.carrierTitle
+        val unit = itemView.context.getString(R.string.baht)
+        carrierTitle.text = DeliveryType.fromString(deliveryOption.methodCode).toString()
         methodTitle.text = deliveryOption.methodTitle
-        if(deliveryOption.available){
+        amount.text = "$unit${deliveryOption.amount}"
+        amount.visibility = if (deliveryOption.amount > 0) View.VISIBLE else View.GONE
+        if (deliveryOption.available) {
             itemView.setOnClickListener {
                 listener?.onSelectedOptionListener(deliveryOption)
             }
--- app/src/main/java/cenergy/central/com/pwb_store/model/DeliveryType.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/model/DeliveryType.kt
@@ -5,12 +5,12 @@ package cenergy.central.com.pwb_store.model
  * Email: Anupharpae@gmail.com
  */
 
-enum class DeliveryType(val key: String) {
+enum class DeliveryType(val methodCode: String) {
     EXPRESS("express") {
         override fun toString(): String = "ส่งด่วน"
     },
     STANDARD("standard") {
-        override fun toString(): String = "จัดส่งแบบปกติ"
+        override fun toString(): String = "ส่งธรรมดา(โดย KERRY)"
     },
     STORE_PICK_UP("storepickup") {
         override fun toString(): String = "รับที่สาขา"
@@ -19,7 +19,7 @@ enum class DeliveryType(val key: String) {
         override fun toString(): String = "กำหนดวันจัดส่ง"
     };
     companion object {
-        private val map = DeliveryType.values().associateBy(DeliveryType::key)
+        private val map = DeliveryType.values().associateBy(DeliveryType::methodCode)
         fun fromString(value: String) = map[value]
     }
 }
