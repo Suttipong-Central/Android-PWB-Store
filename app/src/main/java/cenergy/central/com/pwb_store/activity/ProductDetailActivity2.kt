e15ed4c6f2efc83f60dac4de8f0b646ee95c2301
Pawat Reepim <>
2018-12-20 15:40:16 +0700

    Revert "Fixed errorMessage is null when add product to cart failure"
    This reverts commit f5da4f5ab21ca4793e8d8ea54149ebe0931d3f0d.

--- app/src/main/java/cenergy/central/com/pwb_store/activity/ProductDetailActivity2.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/activity/ProductDetailActivity2.kt
@@ -171,6 +171,7 @@ class ProductDetailActivity2 : AppCompatActivity(), ProductDetailListener, Power
                 if (response != null) {
                     tvNotFound.visibility = View.INVISIBLE
                     containerGroupView.visibility = View.VISIBLE
+
                     // setup product
                     product = response
                     startProductDetailFragment()
@@ -364,15 +365,11 @@ class ProductDetailActivity2 : AppCompatActivity(), ProductDetailListener, Power
 
             override fun failure(error: APIError) {
                 dismissProgressDialog()
+
                 if (error.errorCode == APIError.INTERNAL_SERVER_ERROR.toString()) {
                     showClearCartDialog()
                 } else {
-                    // We found some crash in fabric but we didn't know why errorMessage is null
-                    if(error.errorMessage != null || error.error != null || error.errorUserMessage != null){
-                        showAlertDialog("", error.errorMessage?: error.error?: error.errorUserMessage)
-                    } else {
-                        throw IllegalArgumentException("Some error message is null, Error code is ${error.errorCode}")
-                    }
+                    showAlertDialog("", error.errorMessage)
                 }
             }
         })
