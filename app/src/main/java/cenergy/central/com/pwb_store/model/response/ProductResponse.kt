c6009756c1175778ec8720b1f131bbb6c4e44250
Anuphap Suwannamas <anupharpae@gmail.com>
2019-02-04 10:21:21 +0700

    updated product response model

--- app/src/main/java/cenergy/central/com/pwb_store/model/response/ProductResponse.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/model/response/ProductResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 class ProductResponse(
         @SerializedName("items")
-        var products: ArrayList<Product>? = arrayListOf(),
+        var products: ArrayList<Product> = arrayListOf(),
         var currentPage: Int = 0,
         @SerializedName("total_count")
         var totalCount: Int = 0) {
