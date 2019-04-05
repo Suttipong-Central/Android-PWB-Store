abce22aba04509b9452438be8d17b15ffbae7aae
Pawat Reepim <pawat@apptitude.co.th>
2019-04-05 15:57:36 +0700

    Fixed duplicate payment method

--- app/src/main/java/cenergy/central/com/pwb_store/extensions/Converters.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/extensions/Converters.kt
@@ -5,23 +5,31 @@ import cenergy.central.com.pwb_store.R
 import cenergy.central.com.pwb_store.model.CacheCartItem
 import cenergy.central.com.pwb_store.model.response.PaymentMethod
 
-fun List<CacheCartItem>.getPaymentType(context: Context) : List<PaymentMethod> {
+fun List<CacheCartItem>.getPaymentType(context: Context): List<PaymentMethod> {
     val paymentType = arrayListOf<String>()
     forEach { product ->
-        if (product.paymentMethod.isNotEmpty() && product.paymentMethod != "")  {
+        if (product.paymentMethod.isNotEmpty() && product.paymentMethod != "") {
             paymentType.addAll(product.paymentMethod.split(","))
         }
     }
     val paymentMethodFilter = arrayListOf<PaymentMethod>()
+    val fullPayment = PaymentMethod(title = context.getString(R.string.fullpayment), code = "redirectp2c2p")
+    val installment = PaymentMethod(title = context.getString(R.string.installment), code = "redirect_installment")
     paymentType.distinct().forEach {
-        if (it == "fullpayment" || it == "redirectp2c2p"){
-            paymentMethodFilter.add(PaymentMethod(title = context.getString(R.string.fullpayment), code = "redirectp2c2p"))
+        if (it == "fullpayment" || it == "redirectp2c2p") {
+            if (!paymentMethodFilter.contains(fullPayment)) {
+                paymentMethodFilter.add(fullPayment)
+            }
         }
-        if (it == "installment" || it == "redirect_installment"){
-            paymentMethodFilter.add(PaymentMethod(title = context.getString(R.string.installment), code = "redirect_installment"))
+        if (it == "installment" || it == "redirect_installment") {
+            if (!paymentMethodFilter.contains(installment)) {
+                paymentMethodFilter.add(installment)
+            }
         }
     }
     return paymentMethodFilter
+}
+
 fun List<PaymentMethod>.getMethodTitle(): List<String> {
     val methods = arrayListOf<String>()
     forEach { paymentMethod ->
