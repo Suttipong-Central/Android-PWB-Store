d7f6ad014286b1fe6415346706fc7cf0afab2cb1
Anuphap Suwannamas <anupharpae@gmail.com>
2019-05-13 10:00:51 +0700

    LocalDatabase - Remove getting address from db/ remove about seeder class/ remove raw json

--- app/src/main/java/cenergy/central/com/pwb_store/activity/SplashScreenActivity.java
+++ app/src/main/java/cenergy/central/com/pwb_store/activity/SplashScreenActivity.java
@@ -22,24 +22,6 @@ public class SplashScreenActivity extends AppCompatActivity {
         start();
     }
 
-//    private void storeAddressRawJson() {
-//        // clear database
-//        database.deleteProvinces();
-//        database.deleteDistricts();
-//        database.deleteSubDistricts();
-//        database.deletePostcodes();
-//
-//        // seeder
-//        new ProvinceSeeder(this, database, R.raw.seed_province).seed();
-//        new DistrictSeeder(this, database, R.raw.seed_district).seed();
-//        new SubDistrictSeeder(this, database, R.raw.seed_sub_district).seed();
-//        new PostcodeSeeder(this, database, R.raw.seed_postcode).seed();
-//
-//        preferenceManager.setAddressLoaded(true);
-//
-//        start();
-//    }
-
     private void start() {
         if (database.getUserToken() != null) {
             // start main page
