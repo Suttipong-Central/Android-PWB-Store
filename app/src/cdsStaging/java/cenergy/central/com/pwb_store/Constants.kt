16806fdf8510e69bb3b24f56c4630170fd10b522
Anuphap Suwannamas <anupharpae@gmail.com>
2019-05-14 14:15:59 +0700

    update environment of cds-staging

--- app/src/cdsStaging/java/cenergy/central/com/pwb_store/Constants.kt
+++ app/src/cdsStaging/java/cenergy/central/com/pwb_store/Constants.kt
@@ -5,7 +5,20 @@ import java.util.*
 
 class Constants {
     companion object {
-        // TODO: update environment from CDS
+        // region endpoint
+        const val BASE_URL_MAGENTO = "https://staging-mdc.central.co.th"
+        const val CLIENT_MAGENTO = "Bearer jl7r6o9klp4gl1pt0w6to55hsqhg30nr"
+        const val PWB_HOST_NAME = "staging-mdc.central.co.th"
+        // ens region
+
+        // region central
+        const val CENTRAL_HOST_NAME = "https://uat-api.central.tech"
+        const val CLIENT_SERVICE_NAME = "execute-api"
+        const val CLIENT_REGION = "ap-southeast-1"
+        const val CLIENT_X_API_KEY = "lIrZy8ZTEvkmu4uDe0m06wqNo91REUN7aWnk6GYi"
+        const val CLIENT_ACCESS_KEY = "AKIAIIIW7RXNLIKRTSNQ"
+        const val CLIENT_SECRET_KEY = "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"
+        // end region
 
         // region formatter
         val DATE_FORMATTER_VALUE = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
