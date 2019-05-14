07d1dc13f57472522d8bfde80fc34caa1b909d8f
Anuphap Suwannamas <anupharpae@gmail.com>
2019-05-14 16:40:34 +0700

    Endpoint - update cart service endpoints

--- app/src/main/java/cenergy/central/com/pwb_store/manager/service/CartService.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/manager/service/CartService.kt
@@ -11,15 +11,15 @@ import retrofit2.http.*
 
 internal interface CartService {
 
-    @POST("/{lang}/rest/V1/guest-carts")
+    @POST("/rest/{lang}/V1/guest-carts")
     fun createCart(@Path("lang") language: String): Call<String>
 
-    @POST("/{lang}/rest/V1/guest-carts/{cartId}/items")
+    @POST("/rest/{lang}/V1/guest-carts/{cartId}/items")
     fun addProduct(@Path("lang") language: String,
                    @Path("cartId") cartId: String,
                    @Body cartItemBody: CartItemBody): Call<CartItem>
 
-    @GET("/{lang}/rest/V1/guest-carts/{quoteId}/items")
+    @GET("/rest/{lang}/V1/guest-carts/{quoteId}/items")
     fun viewCart(@Path("lang") language: String,
                  @Path("quoteId") quoteID: String): Call<List<CartItem>>
 
@@ -27,45 +27,42 @@ internal interface CartService {
     fun deleteItem(@Path("cartId") cartId: String,
                    @Path("itemId") itemId: Long): Call<Boolean>
 
-    @PUT("/{lang}/rest/V1/guest-carts/{cartId}/items/{itemId}")
+    @PUT("/rest/{lang}/V1/guest-carts/{cartId}/items/{itemId}")
     fun updateItem(@Path("lang") language: String,
                    @Path("cartId") cartId: String,
                    @Path("itemId") itemId: Long,
                    @Body updateItemBody: UpdateItemBody): Call<CartItem>
 
-    // TODO: TDB - For instigate "Guest Cart"
-//    @POST("/{lang}/rest/V1/headless/guest-carts/{cartId}/estimate-shipping-methods")
-    @POST("/{lang}/rest/V1/guest-carts/{cartId}/estimate-shipping-methods")
+//    @POST("/rest/{lang}/V1/headless/guest-carts/{cartId}/estimate-shipping-methods")
+    @POST("/rest/{lang}/V1/guest-carts/{cartId}/estimate-shipping-methods")
     fun getOrderDeliveryOptions(@Path("lang") language: String,
                                 @Path("cartId") cartId: String,
                                 @Body deliveryBody: DeliveryOptionsBody): Call<List<DeliveryOption>>
 
-    @POST("/{lang}/rest/V1/guest-carts/{cartId}/shipping-information")
+    @POST("/rest/{lang}/V1/guest-carts/{cartId}/shipping-information")
     fun createShippingInformation(@Path("lang") language: String,
                                   @Path("cartId") cartId: String,
                                   @Body shippingBody: ShippingBody): Call<ShippingInformationResponse>
 
     //    @PUT("/rest/V1/guest-carts/{cartId}/order")
-    @POST("/{lang}/rest/V1/headless/guest-carts/{cartId}/payment-information")
+    @POST("/rest/{lang}/V1/headless/guest-carts/{cartId}/payment-information")
     fun updateOrder(@Path("lang") language: String,
                     @Path("cartId") cartId: String,
                     @Body paymentInformation: PaymentInformationBody): Call<String>
 
-    // TODO: TDB - For instigate "Guest Cart"
-    //    @PUT("/rest/V1/guest-carts/{cartId}/order")
-    @POST("/{lang}/rest/V1/guest-carts/{cartId}/payment-information")
+    // @PUT("/rest/V1/guest-carts/{cartId}/order")
+    @POST("/rest/{lang}/V1/guest-carts/{cartId}/payment-information")
     fun updateOrder(@Path("lang") language: String,
                     @Path("cartId") cartId: String,
                     @Body paymentInformation: PaymentInfoBody): Call<String>
 
-    // TODO: TDB - For instigate "Guest Cart"
-    @GET("/{lang}/rest/V1/orders/{orderId}")
+    @GET("/rest/{lang}/V1/orders/{orderId}")
     fun getOrder(
             @Header("Authorization") token: String,
             @Path("lang") language: String,
             @Path("orderId") orderId: String): Call<OrderResponse>
 
-    @GET("/{lang}/rest/all/V1/headless/storepickup")
+    @GET("/rest/{lang}/all/V1/headless/storepickup")
     fun getBranches(@Path("lang") language: String,
                     @Query("searchCriteria[sortOrders][0][field]") orderBy: String,
                     @Query("searchCriteria[sortOrders][0][direction]") direction: String): Call<BranchResponse>
