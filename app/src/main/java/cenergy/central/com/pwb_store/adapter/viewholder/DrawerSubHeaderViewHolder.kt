79cf4ef11cd927313ca74b3e12ef9f792e83a848
Pawat Reepim <pawat@apptitude.co.th>
2019-05-16 14:36:10 +0700

    Separate color from CDS and PWB and Edit drawer sub header for CDS

--- app/src/main/java/cenergy/central/com/pwb_store/adapter/viewholder/DrawerSubHeaderViewHolder.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/adapter/viewholder/DrawerSubHeaderViewHolder.kt
@@ -3,19 +3,13 @@ package cenergy.central.com.pwb_store.adapter.viewholder
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
-import android.widget.LinearLayout
 import cenergy.central.com.pwb_store.R
 import cenergy.central.com.pwb_store.extensions.setImage
 import cenergy.central.com.pwb_store.view.PowerBuyTextView
 
-/**
- * Created by napabhat on 9/19/2017 AD.
- */
-
 class DrawerSubHeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private var mIcon = itemView.findViewById<ImageView>(R.id.icon)
     private var mTitle = itemView.findViewById<PowerBuyTextView>(R.id.title)
-    private var mLinearLayout = itemView.findViewById<LinearLayout>(R.id.layout_compare)
 
     fun bindItem(title: String) {
         mTitle.text = title
@@ -35,9 +29,5 @@ class DrawerSubHeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVi
                 mIcon.setImage(R.drawable.ic_logout)
             }
         }
-    }
-
-    companion object {
-        private val TAG = DrawerSubHeaderViewHolder::class.java.simpleName
     }
 }
