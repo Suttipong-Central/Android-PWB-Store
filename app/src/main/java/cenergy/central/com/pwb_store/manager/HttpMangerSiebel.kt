3ddeeac041fb0620000d42bc15b6f5684b7266ec
Anuphap Suwannamas <anupharpae@gmail.com>
2019-09-16 11:27:45 +0700

    Fixed api manager of central siebel

--- app/src/main/java/cenergy/central/com/pwb_store/manager/HttpMangerSiebel.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/manager/HttpMangerSiebel.kt
@@ -3,6 +3,7 @@ package cenergy.central.com.pwb_store.manager
 import android.annotation.SuppressLint
 import android.content.Context
 import cenergy.central.com.pwb_store.BuildConfig
+import cenergy.central.com.pwb_store.Constants
 import cenergy.central.com.pwb_store.manager.service.MemberService
 import cenergy.central.com.pwb_store.model.APIError
 import cenergy.central.com.pwb_store.model.Member
@@ -30,8 +31,8 @@ class HttpMangerSiebel(context: Context) {
     init {
         val session = auth()
         val awsCredentialsProvider = PwbAWSCredentialsProvider(session)
-        val awsInterceptor = AwsInterceptor(awsCredentialsProvider, CLIENT_SERVICE_NAME,
-                CLIENT_REGION, CLIENT_X_API_KEY)
+        val awsInterceptor = AwsInterceptor(awsCredentialsProvider, Constants.CLIENT_SERVICE_NAME,
+                Constants.CLIENT_REGION, Constants.CLIENT_X_API_KEY)
         val interceptor = HttpLoggingInterceptor()
         if (BuildConfig.DEBUG) interceptor.level = HttpLoggingInterceptor.Level.BODY
         val defaultHttpClient = OkHttpClient.Builder()
@@ -48,7 +49,7 @@ class HttpMangerSiebel(context: Context) {
                 .build()
 
         retrofit = Retrofit.Builder()
-                .baseUrl(HOST_NAME)
+                .baseUrl(Constants.CENTRAL_HOST_NAME)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(defaultHttpClient)
                 .build()
@@ -104,8 +105,8 @@ class HttpMangerSiebel(context: Context) {
 
     private fun auth(): Session {
         val auth = Session()
-        auth.accessKey = CLIENT_ACCESS_KEY
-        auth.secretKey = CLIENT_SECRET_KEY
+        auth.accessKey = Constants.CLIENT_ACCESS_KEY
+        auth.secretKey = Constants.CLIENT_SECRET_KEY
 
         return auth
     }
@@ -116,13 +117,6 @@ class HttpMangerSiebel(context: Context) {
     }
 
     companion object {
-        //        //Specific Client
-        private const val CLIENT_SERVICE_NAME = "execute-api"
-        private const val CLIENT_REGION = "ap-southeast-1"
-        private const val CLIENT_X_API_KEY = "lIrZy8ZTEvkmu4uDe0m06wqNo91REUN7aWnk6GYi"
-        private const val HOST_NAME = "https://api.central.tech"
-        private const val CLIENT_ACCESS_KEY = "AKIAJK27ORQKY42QRFWQ"
-        private const val CLIENT_SECRET_KEY = "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"
 
         @SuppressLint("StaticFieldLeak")
         private var instance: HttpMangerSiebel? = null
--- app/src/pwbStaging/java/cenergy/central/com/pwb_store/Constants.kt
+++ app/src/pwbStaging/java/cenergy/central/com/pwb_store/Constants.kt
@@ -7,26 +7,10 @@ class Constants {
     companion object {
 
         // region magento
-//        const val BASE_URL_MAGENTO = "https://staging.powerbuy.co.th"
-//        const val CLIENT_MAGENTO = "Bearer js6rsdr0anv25ubgcqixh5m5mckbbwth"
-//        const val PWB_HOST_NAME = "staging.powerbuy.co.th"
         const val BASE_URL_MAGENTO = "https://staging-mdc.powerbuy.co.th"
         const val PWB_HOST_NAME = "staging-mdc.powerbuy.co.th"
         const val CLIENT_MAGENTO = "Bearer ngvids7tnggs94sm81k8a3oxjgl9cd16"
         // endregion
-
-        // for testing
-//        const val BASE_URL_MAGENTO = "https://uat.powerbuy.co.th"
-//        const val CLIENT_MAGENTO = "Bearer ngvids7tnggs94sm81k8a3oxjgl9cd16"
-//        const val PWB_HOST_NAME = "uat.powerbuy.co.th"
-        // endregion
-
-//         const val CLIENT_SERVICE_NAME = "execute-api"
-//         const val CLIENT_REGION = "ap-southeast-1"
-//         const val CLIENT_X_API_KEY = "lIrZy8ZTEvkmu4uDe0m06wqNo91REUN7aWnk6GYi"
-//         const val HOST_NAME = "https://sit-api.central.tech"
-//         const val CLIENT_ACCESS_KEY = "AKIAIZBEAJOWKANSLLYA"
-//         const val CLIENT_SECRET_KEY = "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"
 
         const val CONTENT_TYPE = "Content-Type"
         const val APPTICATION_JSON = "application/json"
