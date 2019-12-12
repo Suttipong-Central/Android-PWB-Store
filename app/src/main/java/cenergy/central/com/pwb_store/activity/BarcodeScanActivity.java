e01c448c8cb8fd26f16157e33eb589173850f8f4
Pawat Reepim <pawat@apptitude.co.th>
2019-12-12 15:14:42 +0700

    Fix BarcodeScanActivity not use butterknife

--- app/src/main/java/cenergy/central/com/pwb_store/activity/BarcodeScanActivity.java
+++ app/src/main/java/cenergy/central/com/pwb_store/activity/BarcodeScanActivity.java
@@ -2,53 +2,42 @@ package cenergy.central.com.pwb_store.activity;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.CompoundBarcodeView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import cenergy.central.com.pwb_store.R;
 import cenergy.central.com.pwb_store.manager.Contextor;
 
 public class BarcodeScanActivity extends AppCompatActivity {
 
-    @BindView(R.id.image_button_back)
     ImageButton mImageButtonBack;
-
-    @BindView(R.id.image_button_flashlight)
     ImageButton mFlashLightButton;
-
-    @BindView(R.id.zxing_barcode_scanner)
     CompoundBarcodeView mCompoundBarcodeView;
-
     private CaptureManager captureManager;
-
     private static boolean cameraOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode_scan);
-        ButterKnife.bind(this);
+        mCompoundBarcodeView = findViewById(R.id.zxing_barcode_scanner);
+        mImageButtonBack = findViewById(R.id.image_button_back);
+        mFlashLightButton = findViewById(R.id.image_button_flashlight);
 
-//        mCompoundBarcodeView.setStatusText(getString(R.string.barcode_status));
+        //mCompoundBarcodeView.setStatusText(getString(R.string.barcode_status));
         mCompoundBarcodeView.getBarcodeView().getCameraSettings().setExposureEnabled(true);
         mCompoundBarcodeView.getBarcodeView().getCameraSettings().setMeteringEnabled(true);
         mCompoundBarcodeView.getBarcodeView().getCameraSettings().setScanInverted(true);
         mCompoundBarcodeView.getBarcodeView().getCameraSettings().setBarcodeSceneModeEnabled(true);
         mCompoundBarcodeView.getBarcodeView().getCameraSettings().setContinuousFocusEnabled(true);
 
-        mImageButtonBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
-
+        mImageButtonBack.setOnClickListener(v -> onBackPressed());
 
         if (!hasFlash()){
             mFlashLightButton.setVisibility(View.GONE);
@@ -57,19 +46,16 @@ public class BarcodeScanActivity extends AppCompatActivity {
         captureManager = new CaptureManager(this, mCompoundBarcodeView);
         captureManager.initializeFromIntent(getIntent(), savedInstanceState);
         captureManager.decode();
-        mFlashLightButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!cameraOn){
-                    mFlashLightButton.setImageResource(R.drawable.ic_flash_off);
-                    mCompoundBarcodeView.getBarcodeView().setTorch(true);
-                    cameraOn = true;
-                }
-                else {
-                    mFlashLightButton.setImageResource(R.drawable.ic_flash_on);
-                    mCompoundBarcodeView.getBarcodeView().setTorch(false);
-                    cameraOn = false;
-                }
+        mFlashLightButton.setOnClickListener(v -> {
+            if (!cameraOn){
+                mFlashLightButton.setImageResource(R.drawable.ic_flash_off);
+                mCompoundBarcodeView.getBarcodeView().setTorch(true);
+                cameraOn = true;
+            }
+            else {
+                mFlashLightButton.setImageResource(R.drawable.ic_flash_on);
+                mCompoundBarcodeView.getBarcodeView().setTorch(false);
+                cameraOn = false;
             }
         });
     }
@@ -98,7 +84,7 @@ public class BarcodeScanActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
         captureManager.onSaveInstanceState(outState);
         super.onSaveInstanceState(outState);
     }
