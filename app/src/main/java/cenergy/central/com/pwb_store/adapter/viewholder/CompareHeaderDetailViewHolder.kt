2e7fd0e31020b50c5c404247af553afc34b2ecc4
Pawat Reepim <pawat@apptitude.co.th>
2020-03-18 09:52:13 +0700

    Fix force compare header title "Short Description" change to "Key Features"

--- app/src/main/java/cenergy/central/com/pwb_store/adapter/viewholder/CompareHeaderDetailViewHolder.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/adapter/viewholder/CompareHeaderDetailViewHolder.kt
@@ -2,12 +2,17 @@ package cenergy.central.com.pwb_store.adapter.viewholder
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import cenergy.central.com.pwb_store.R
 import cenergy.central.com.pwb_store.adapter.CompareDetailAdapter
 import kotlinx.android.synthetic.main.list_item_text_header_compare_detail.view.*
 
 class CompareHeaderDetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val tvLabel = itemView.labelTextView
     fun bind(item: CompareDetailAdapter.CompareTitleItem) {
-        tvLabel.text = item.title
+        if (item.title == "Short Description"){
+            tvLabel.text = itemView.context.getString(R.string.product_overview)
+        } else {
+            tvLabel.text = item.title
+        }
     }
 }
