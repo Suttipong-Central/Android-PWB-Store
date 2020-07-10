0bb24608c04cba2a9ab0018e4ac6b29f9e07dfec
Pawat Reepim <pawat@apptitude.co.th>
2020-07-10 16:34:33 +0700

    EOR-45 Add start new activity when press item

--- app/src/main/java/cenergy/central/com/pwb_store/adapter/DisplayPromotionAdapter.kt
+++ app/src/main/java/cenergy/central/com/pwb_store/adapter/DisplayPromotionAdapter.kt
@@ -1,13 +1,16 @@
 package cenergy.central.com.pwb_store.adapter
 
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import cenergy.central.com.pwb_store.R
+import cenergy.central.com.pwb_store.activity.ProductDetailActivity
 import cenergy.central.com.pwb_store.extensions.setImageUrl
 import cenergy.central.com.pwb_store.model.Product
+import cenergy.central.com.pwb_store.utils.Analytics
 import cenergy.central.com.pwb_store.view.PowerBuyTextView
 
 class DisplayPromotionAdapter : RecyclerView.Adapter<DisplayPromotionViewHolder>() {
@@ -33,13 +36,26 @@ class DisplayPromotionViewHolder(itemView: View): RecyclerView.ViewHolder(itemVi
     private val productImg = itemView.findViewById<ImageView>(R.id.imgProduct)
     private val productName = itemView.findViewById<PowerBuyTextView>(R.id.freebieNameTv)
     private val outOfStockTv = itemView.findViewById<PowerBuyTextView>(R.id.tvOutOfStock)
+    private var clicked = true
+
     fun bind(product: Product) {
+        val context = itemView.context
         productImg.setImageUrl(product.getImageUrl())
         productName.text = product.name
         if (product.extension?.stokeItem != null &&  product.extension!!.stokeItem!!.isInStock){
             outOfStockTv.visibility = View.VISIBLE
         } else {
             outOfStockTv.visibility = View.GONE
+        }
+        itemView.setOnClickListener {
+            if (clicked) {
+                clicked = false
+                val analytics = Analytics(context)
+                analytics.trackViewItem(product.sku)
+                ProductDetailActivity.startActivity(context, product.sku,
+                        product.getPricePerStore())
+                Handler().postDelayed({ clicked = true }, 1000)
+            }
         }
     }
 }
